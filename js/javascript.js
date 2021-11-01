c8aa0771695908f43dec08836cb3002532536733
juani19 <89429217+juani19@users.noreply.github.com>
2021-11-01 15:04:34 -0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,5 @@
 function cambiarfondo1() { document.body.style.backgroundColor = "#212F3D";
  document.body.style.color = "white"; 
-  document.getElementById("card").innerHTML.backgroundColor="black"; 
    }
 function cartel() {alert("PAGINA DISEÑADA POR JUAN CRUZ SCLAVO")}
 
